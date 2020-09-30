aee60ff1a3f15da018b93f0eb782f862943ab257
Wasun Patrut <wasun26@hotmail.com>
2020-09-30 18:47:38 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,28 +1,33 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
     <div class="container">
         <div class="row">
             <div class="col">
                 <div class="card">
                     <div class="card-header text-primary">
-                    <a href="#">
-                    <center><h5>สำหรับอาจารย์</h5></center>
-                    </a>
-                    </div>
-                        <a href="login.php">
-                        <button type="button" class="btn btn-succes">Login</button>
+                        <a href="#">
+                            <center>
+                                <h5>สำหรับอาจารย์</h5>
+                            </center>
                         </a>
-                   
+                    </div>
+                    <a href="login.php">
+                        <button type="button" class="btn btn-succes">Login</button>
+                    </a>
+
                 </div>
             </div>
             <div class="col">
                 <div class="card">
                     <div class="card-header text-primary">
-                        <center><h5>สำหรับเจ้าหน้าที่</h5></center>
+                        <center>
+                            <h5>สำหรับเจ้าหน้าที่</h5>
+                        </center>
                     </div>
                     <button type="button" class="btn btn-success">Login</button>
                 </div>
@@ -30,7 +35,9 @@
             <div class="col">
                 <div class="card">
                     <div class="card-header text-primary">
-                    <center><h5>สำหรับนักศึกษา</h5></center>
+                        <center>
+                            <h5>สำหรับนักศึกษา</h5>
+                        </center>
                     </div>
                     <button type="button" class="btn btn-success">Login</button>
                 </div>
