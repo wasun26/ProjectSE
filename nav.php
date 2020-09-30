37efbe1b63e8dae12c58020bc9433cae1b065252
administer03 <tanad_s@cmu.ac.th>
2020-09-30 17:48:09 +0700

    after login

--- nav.php
+++ nav.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="nav.css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+</head>
+<body>
+
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
 	<div class="container-fluid">
 		<a href="./" class="navbar-brand">
@@ -38,3 +50,5 @@
 		</div>
 	</div>
 </nav>
+</body>
+</html>
