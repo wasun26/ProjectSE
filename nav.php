f372623d0315c795bfe9bfad641a782bac016c8f
Wasun  Patrut <wasun26@hotmail.com>
2020-10-12 18:09:40 +0700

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,10 +2,10 @@
 <html>
 
 <head>
-	<link rel="stylesheet" href="#">
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -26,7 +26,12 @@
 				<ul class="navbar-nav mr-auto">
 					<li class="nav-item"><a href="./" class="nav-link" routerLink="train">หน้าแรก</a></li>
 					<?php
-
+					#$conn = new mysqli($config['hostname'], $config['dbuser'], $config['dbpassword'], $config['dbname']);
+					#$email = $_POST['email'] . '@cmu.ac.th';
+					#$login = "SELECT email, password FROM $type WHERE email='$email'";
+					#$result = $conn->query($login);
+					#$dbarr = $result->fetch_assoc();
+					#$conn->close();
 					if (false) {
 						echo ("
 						<li class='nav-item'><a href='#' class='nav-link' routerLink='profile'>โปรไฟล์</a></li>
