88fa08c05f8b67fe3f90bb3366597f40d2192d3a
Wasun  Patrut <wasun26@hotmail.com>
2020-10-12 12:51:42 +0700

    12.51

--- insert.php
+++ insert.php
@@ -1,52 +1,42 @@
 <head>
-<meta charset="UTF-8">
+  <meta charset="UTF-8">
 </head>
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "se";
-
-  //Receive data from the previous form
-    $phase = $_POST['phase'];
-    $year = $_POST['year'];
-    $semester = $_POST['semester'];
-    $subject = $_POST['subject'];
-    $date = $_POST['date'];
-    $room = $_POST['room'];
-    $time = $_POST['time'];
-    $examiner_t = $_POST['examiner_t'];
-    $examiner_s = $_POST['examiner_s'];
+include("config.php");
+$phase = $_POST['phase'];
+$year = $_POST['year']-543;
+$semester = $_POST['semester'];
+$subject = $_POST['subject'];
+$date = $_POST['date'];
+$room = $_POST['room'];
+$time = $_POST['time'];
+$examiner_t = $_POST['examiner_t'];
+$examiner_s = $_POST['examiner_s'];
 
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn -> connect_error);
-    } 
-    
-    mysqli_set_charset($conn, "utf8");
+// Create connection
+$conn = new mysqli(
+  $config['hostname'],
+  $config['dbuser'],
+  $config['dbpassword'],
+  $config['dbname']
+);
 
-    //Connected successfully
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
 
-    $sql = "INSERT INTO exam (phase, subject, semester, year, date, time, room, examiner_t, examiner_s) VALUES  
+mysqli_set_charset($conn, "utf8");
+
+//Connected successfully
+
+$sql = "INSERT INTO exam (phase, subject, semester, year, date, time, room, examiner_t, examiner_s) VALUES  
                  ('$phase', '$subject', '$semester', '$year', '$date', '$time', '$room', '$examiner_t', '$examiner_s')";
 
-    $conn -> query($sql);
-    $conn -> close();
+$conn->query($sql);
+$conn->close();
 
-    echo "เพิ่มข้อมูลเรียบร้อยแล้ว <br><br>"; 
-	echo "<a href='insert.html'>เพิ่มข้อมูลถัดไป</a>"
+echo "เพิ่มข้อมูลเรียบร้อยแล้ว <br><br>";
+echo "<a href='insert.html'>เพิ่มข้อมูลถัดไป</a>"
 ?>
-
-
-
-
-
-
-		
-
-
-
--- staff.php
+++ staff.php
@@ -1,3 +1,7 @@
+<!DOCTYPE html>
+<?php
+include("config.php");
+?>
 <html>
 
 <head>
@@ -5,20 +9,21 @@
 </head>
 
 <body>
-<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "se";
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
+	<?php
+
+	$conn = new mysqli(
+		$config['hostname'],
+		$config['dbuser'],
+		$config['dbpassword'],
+		$config['dbname']
+	);
+
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
-	} 
+	}
 
 	mysqli_set_charset($conn, "utf8");
-?>
+	?>
 	<div class="container">
 		<h1>เพิ่มวิชาสอบ</h1>
 		<form action="insert.php" method="post">
@@ -27,95 +32,95 @@
 
 			ภาคการศึกษา: <input type="text" name="semester"><br>
 
-			<label for = "phase">ช่วงสอบ:</label>
-			<select name = "phase" id = "phase">
-			<option value = "NULL">เลือก</option>
-			<option value = "mid">Midterm</option>
-			<option value = "final">Final</option>
+			<label for="phase">ช่วงสอบ:</label>
+			<select name="phase" id="phase">
+				<option value="NULL">เลือก</option>
+				<option value="mid">Midterm</option>
+				<option value="final">Final</option>
 			</select><br>
 
 			<?php
-			    $sql = "SELECT * FROM subject ";
-				$result =$conn->query($sql);
-				echo "วิชา: ";
-				echo "<select name='subject'>";
-				  
-				echo "<option value = 'NULL'> เลือก </option>";
-				while ($row=$result->fetch_assoc())    { //begin while
-					$code = $row['id'];
-					echo "<option value = '$code'> $code </option>";
-				}  //end while
-	  
-				echo "</select><br>";
+			$sql = "SELECT * FROM subject ";
+			$result = $conn->query($sql);
+			echo "วิชา: ";
+			echo "<select name='subject'>";
+
+			echo "<option value = 'NULL'> เลือก </option>";
+			while ($row = $result->fetch_assoc()) { //begin while
+				$code = $row['id'];
+				echo "<option value = '$code'> $code </option>";
+			}  //end while
+
+			echo "</select><br>";
 			?>
 
-			<label for = "date">วันที่:</label>
+			<label for="date">วันที่:</label>
 			<input type="date" id="date" name="date"><br>
 
 			<?php
-			    $sql = "SELECT * FROM room ";
-				$result =$conn->query($sql);
-				echo "ห้องสอบ: ";
-				echo "<select name='room'>";
-				  
-				echo "<option value = 'NULL'> เลือก </option>";
-				while ($row=$result->fetch_assoc())    { //begin while
-					$name = $row['name'];
-					echo "<option value = '$name'> $name </option>";
-				}  //end while
-	  
-				echo "</select><br>";
+			$sql = "SELECT * FROM room ";
+			$result = $conn->query($sql);
+			echo "ห้องสอบ: ";
+			echo "<select name='room'>";
+
+			echo "<option value = 'NULL'> เลือก </option>";
+			while ($row = $result->fetch_assoc()) { //begin while
+				$name = $row['name'];
+				echo "<option value = '$name'> $name </option>";
+			}  //end while
+
+			echo "</select><br>";
 			?>
 
 			<?php
-			    $sql = "SELECT * FROM timeexam ";
-				$result =$conn->query($sql);
-				echo "เวลาสอบ: ";
-				echo "<select name='time'>";
-				  
-				echo "<option value = 'NULL'> เลือก </option>";
-				while ($row=$result->fetch_assoc())    { //begin while
-					$time = $row['id'];
-					$time_start = $row['timeStart'];
-					$time_end = $row['timeFinish'];
-					echo "<option value = '$time'> $time_start - $time_end</option>";
-				}  //end while
-	  
-				echo "</select><br>";
+			$sql = "SELECT * FROM timeexam ";
+			$result = $conn->query($sql);
+			echo "เวลาสอบ: ";
+			echo "<select name='time'>";
+
+			echo "<option value = 'NULL'> เลือก </option>";
+			while ($row = $result->fetch_assoc()) { //begin while
+				$time = $row['id'];
+				$time_start = $row['timeStart'];
+				$time_end = $row['timeFinish'];
+				echo "<option value = '$time'> $time_start - $time_end</option>";
+			}  //end while
+
+			echo "</select><br>";
 			?>
 
 			<?php
-			    $sql = "SELECT * FROM teacher ";
-				$result =$conn->query($sql);
-				echo "ผู้คุมสอบ(อาจารย์): ";
-				echo "<select name='examiner_t'>";
-				  
-				echo "<option value = 'NULL'> เลือก </option>";
-				while ($row=$result->fetch_assoc())    { //begin while
-					$id = $row['id'];
-					$f_name = $row['fname'];
-					$l_name = $row['lname'];
-					echo "<option value = '$id'>$f_name $l_name</option>";
-				}  //end while
-	  
-				echo "</select><br>";
+			$sql = "SELECT * FROM teacher ";
+			$result = $conn->query($sql);
+			echo "ผู้คุมสอบ(อาจารย์): ";
+			echo "<select name='examiner_t'>";
+
+			echo "<option value = 'NULL'> เลือก </option>";
+			while ($row = $result->fetch_assoc()) { //begin while
+				$id = $row['id'];
+				$f_name = $row['fname'];
+				$l_name = $row['lname'];
+				echo "<option value = '$id'>$f_name $l_name</option>";
+			}  //end while
+
+			echo "</select><br>";
 			?>
 
-<?php
-			    $sql = "SELECT * FROM staff ";
-				$result =$conn->query($sql);
-				echo "ผู้คุมสอบ(บุคลากร): ";
-				echo "<select name='examiner_s'>";
-				  
-				echo "<option value = 'NULL'> เลือก </option>";
-				while ($row=$result->fetch_assoc())    { //begin while
-					$id = $row['id'];
-					$f_name = $row['fname'];
-					$l_name = $row['lname'];
-					echo "<option value = '$id'>$f_name $l_name</option>";
-				}  //end while
-	  
-				echo "</select><br><br>";
+			<?php
+			$sql = "SELECT * FROM staff ";
+			$result = $conn->query($sql);
+			echo "ผู้คุมสอบ(บุคลากร): ";
+			echo "<select name='examiner_s'>";
+
+			echo "<option value = 'NULL'> เลือก </option>";
+			while ($row = $result->fetch_assoc()) { //begin while
+				$id = $row['id'];
+				$f_name = $row['fname'];
+				$l_name = $row['lname'];
+				echo "<option value = '$id'>$f_name $l_name</option>";
+			}  //end while
+
+			echo "</select><br><br>";
 			?>
 			<input type="submit" value="Add">&nbsp&nbsp
 			<input type="reset" value="Reset">
