812265da6baf18b1ced2c2d5bb24d43eca326feb
Wasun  Patrut <wasun26@hotmail.com>
2020-10-13 14:09:19 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,12 @@
 <?php
-session_start();
 session_destroy();
 
 if (!(isset($_SESSION['login_true']))) {
     header("Location: ./?");
 	exit;
 }
-
-#<meta http-equiv=refresh content=1;URL=?>
+?>
+<meta http-equiv=refresh content=1;URL=?>
 <div class="container justify-content-center">
 <b>ออกจากระบบเรียบร้อย</b><br>
 กำลังนำกลับสู่หน้าหลัก...
