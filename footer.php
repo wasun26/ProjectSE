05b872ea5c911e336f67836f797d54f050afcfe5
SitthaTime <49423962+SitthaTime@users.noreply.github.com>
2020-10-15 20:43:47 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
         <p class="pr-5 text-white-50">คณะวิทยาศาสตร์ มหาวิทยาลัยเชียงใหม่</p>
         <p class="pr-5 text-white-50">239 ถ. ห้วยแก้ว ต.สุเทพ อ. เมือง จ. เชียงใหม่ 50200</p>
         <p class="pr-5 text-white-50">โทรศัพท์ : 0-5394-3412-16 ธุรการ : กด 0 หรือ 101 </p><br>
-        <p><a href="#"><i class="fa fa-facebook-square mr-1"></i></a><a href="#"><i class="fa fa-linkedin-square"></i></a></p>
+        <p><a href="https://www.facebook.com/compscicmu/"><i class="fa fa-facebook-square mr-1"></i></a></p>
       </div>
 
       <div class="col about-company">
