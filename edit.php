80c8466d875c548bf03d7f2e26d48c435dd9501f
puripat-cmu <64116906+puripat-cmu@users.noreply.github.com>
2020-10-15 21:17:00 +0700

    edit page

---
+++ edit.php
@@ -0,0 +1,45 @@
+<head>
+  <meta charset="UTF-8">
+</head>
+<?php
+include("config.php");
+$id = $_POST['id'];
+$phase = $_POST['phase'];
+$semester = $_POST['semester'];
+$date = $_POST['date'];
+$room = $_POST['room'];
+$time = $_POST['time'];
+$examiner_t = $_POST['examiner_t'];
+$examiner_s = $_POST['examiner_s'];
+
+// Create connection  
+$conn = new mysqli(
+  $config['hostname'],
+  $config['dbuser'],
+  $config['dbpassword'],
+  $config['dbname']
+);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+mysqli_set_charset($conn, "utf8");
+
+$sql = "UPDATE `exam` SET `phase` = '$phase', 
+ `year` = '$phase',  `date`  = '$date', 
+ `time`  = '$time', 
+ `room`  = '$room', 
+ `semester`  = '$semester',
+ `examiner_t`  = '$examiner_t', 
+ `examiner_s`  = '$examiner_s' 
+ WHERE  exam.id  = '$id'" ;
+
+$conn->query($sql);
+$conn->close();
+
+if ($sql) {
+    
+    echo "แก้ไขเสร็จแล้ว <br><br>";
+}
+?>
