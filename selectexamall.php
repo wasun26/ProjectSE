605aae12aa13cc3f92b9151c58eec0be29b6b882
Wasun  Patrut <wasun26@hotmail.com>
2020-10-17 01:43:32 +0700

    Update selectexamall.php

--- selectexamall.php
+++ selectexamall.php
@@ -7,29 +7,34 @@ $sql = "SELECT DISTINCT S.year
 $result = $conn->query($sql);
 ?><br>
 <div class="form-inline d-flex justify-content-center">
-    <form action="?page=examlist" method="POST">
-        <select name="phase" class="form-control">
-            <option value="1">Midterm</option>
-            <option value="2">Final</option>
-        </select>
-        <select name="semester" class="form-control">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">Summer</option>
-        </select>
-        <select name="year" class="form-control">
-            <?php
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    echo ("<option value=");
-                    echo ($row['year'] . ">");
-                    echo ($row['year'] . "</option>");
-                }
-            }
-            ?>
-        </select>
-        <input type="hidden" name="searchType" value="byterm">
-        <input type="hidden" name="searchData" value="0">
-        <button type="submit" value="ค้นหา" class="btn btn-primary">ค้นหา</button>
-    </form>
+    <div class="card">
+        <div class="card-header text-light" style="background-color: #152F4F;">ค้นหารายเทอม</div>
+        <div class="card-body">
+            <form action="?page=examlist" method="POST">
+                <select name="phase" class="form-control">
+                    <option value="1">Midterm</option>
+                    <option value="2">Final</option>
+                </select>
+                <select name="semester" class="form-control">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">Summer</option>
+                </select>
+                <select name="year" class="form-control">
+                    <?php
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo ("<option value=");
+                            echo ($row['year'] . ">");
+                            echo ($row['year'] . "</option>");
+                        }
+                    }
+                    ?>
+                </select>
+                <input type="hidden" name="searchType" value="byterm">
+                <input type="hidden" name="searchData" value="0">
+                <button type="submit" value="ค้นหา" class="btn btn-primary">ค้นหา</button>
+            </form>
+        </div>
+    </div>
 </div>
