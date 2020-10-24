bd95cbd62d482771092fac8624af265e845bdafc
SitthaTime <49423962+SitthaTime@users.noreply.github.com>
2020-10-24 21:14:37 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -20,6 +20,10 @@ $examiner_t = $_POST['examiner_t'];
 $examiner_s = $_POST['examiner_s'];
 $owner_id = $idUser;
 $state = 0;
+$fail_1 = 0;
+$fail_2 = 0;
+$fail_3 = 0;
+$fail_4 = 0;
 
 $conn = new mysqli(
   $config['hostname'],
@@ -48,10 +52,6 @@ if ($result->num_rows > 0) {
     $time_db = $row['time'];
     $examiner_t_db = $row['examiner_t'];
     $examiner_s_db = $row['examiner_s'];
-    $fail_1 = 0;
-    $fail_2 = 0;
-    $fail_3 = 0;
-    $fail_4 = 0;
 
     if ($subject == $subject_db) {
       $state = 1;
@@ -69,42 +69,22 @@ if ($result->num_rows > 0) {
       $state = 1;
       $fail_4 = 1;
     }
-    if ($state == 1){
-      echo ("<div class='swal2-icon swal2-error swal2-animate-error-icon' style='display: flex;'>
-      <span class='swal2-x-mark'>
-      <span class='swal2-x-mark-line-left'></span>
-      <span class='swal2-x-mark-line-right'></span>
-      </span>
-      </div>");
-      if($fail_1 == 1){
-        echo "<b>วิชา $subject</b> ได้ถูกลงทะเบียนแล้ว<br>";
-      }
-      if ($fail_2 == 1){
-        echo "<b>ห้อง $room</b> ถูกใช้แล้ว<br>";
-      }
-      if ($fail_3 == 1){
-        echo "<b>ผู้คุมสอบ(อาจารย์) $examiner_t</b> มีหน้าที่ในเวลานี้แล้ว<br>";
-      }
-      if ($fail_4 == 1){
-        echo "<b>ผู้คุมสอบ(บุคลากร) $examiner_s</b> มีหน้าที่ในเวลานี้แล้ว<br>";
-      }
-    }
-    break;
   }
-} elseif ($state == 0) {
-  mysqli_set_charset($conn, "utf8");
-  if ($examiner_s == 'NULL') {
-    $sql = "INSERT INTO `exam` (`id`, `phase`, `subject`, `year`, `semester`, `date`, `time`, `room`, `examiner_t`, `examiner_s`, `ownerID`) VALUES  
-                               (NULL, '$phase', '$subject', '$year', '$semester', '$date', '$time', '$room', '$examiner_t', NULL, '$owner_id')";
-  } else {
-    $sql = "INSERT INTO `exam` (`id`, `phase`, `subject`, `year`, `semester`, `date`, `time`, `room`, `examiner_t`, `examiner_s`, `ownerID`) VALUES  
-                               (NULL, '$phase', '$subject', '$year', '$semester', '$date', '$time', '$room', '$examiner_t', '$examiner_s', '$owner_id')";
-  }
-  $conn->query($sql);
 }
-$conn->close();
 ?>
-<?php if ($state == 0) { ?>
+
+<?php if ($state == 0) { 
+   mysqli_set_charset($conn, "utf8");
+   if ($examiner_s == 'NULL') {
+     $sql = "INSERT INTO `exam` (`id`, `phase`, `subject`, `year`, `semester`, `date`, `time`, `room`, `examiner_t`, `examiner_s`, `ownerID`)
+             VALUES (NULL, '$phase', '$subject', '$year', '$semester', '$date', '$time', '$room', '$examiner_t', NULL, '$owner_id')";
+   } else {
+     $sql = "INSERT INTO `exam` (`id`, `phase`, `subject`, `year`, `semester`, `date`, `time`, `room`, `examiner_t`, `examiner_s`, `ownerID`)
+            VALUES (NULL, '$phase', '$subject', '$year', '$semester', '$date', '$time', '$room', '$examiner_t', '$examiner_s', '$owner_id')";
+   }
+   $conn->query($sql);
+?>
+
 <div class="swal2-icon swal2-success swal2-animate-success-icon" style="display: flex;">
   <div class="swal2-success-circular-line-left" style="background-color: rgb(255, 255, 255);"></div>
   <span class="swal2-success-line-tip"></span>
@@ -114,5 +94,26 @@ $conn->close();
   <div class="swal2-success-circular-line-right" style="background-color: rgb(255, 255, 255);"></div>
 </div>
 เพิ่มข้อมูลเรียบร้อยแล้ว<br>
-<?php } ?>
+<?php 
+}else{
+  echo (" <div class='swal2-icon swal2-error swal2-animate-error-icon' style='display: flex;'>
+          <span class='swal2-x-mark'>
+          <span class='swal2-x-mark-line-left'></span>
+          <span class='swal2-x-mark-line-right'></span>
+          </span>
+          </div>");
+  if($fail_1 == 1){
+    echo "<b>วิชา $subject</b> ได้ถูกลงทะเบียนแล้ว<br>";
+  }
+  if ($fail_2 == 1){
+    echo "<b>ห้อง $room</b> ถูกใช้แล้ว<br>";
+  }
+  if ($fail_3 == 1){
+    echo "<b>ผู้คุมสอบ(อาจารย์)</b> มีหน้าที่ในเวลานี้แล้ว<br>";
+  }
+  if ($fail_4 == 1){
+    echo "<b>ผู้คุมสอบ(บุคลากร)</b> มีหน้าที่ในเวลานี้แล้ว<br>";
+  }
+} ?>
+<br>
 <a href='?page=staff' class="btn btn-primary">เพิ่มข้อมูล</a>&nbsp;&nbsp;&nbsp;<a href='?page=main' class="btn btn-primary">กลับไปหน้าหลัก</a>
