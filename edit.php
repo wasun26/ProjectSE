78bc1cf14c53918db2d4440638d39a1514030acc
Wasun  Patrut <wasun26@hotmail.com>
2020-10-24 21:28:04 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -87,12 +87,14 @@ $conn->query($sql);
 $conn->close();
 
 if ($sql and $update == TRUE) {
-  echo "<div class='swal2-icon swal2-error swal2-animate-error-icon' style='display: flex;'>
-    <span class='swal2-x-mark'>
-    <span class='swal2-x-mark-line-left'></span>
-    <span class='swal2-x-mark-line-right'></span>
-    </span>
-    </div>
+  echo "<div class='swal2-icon swal2-success swal2-animate-success-icon' style='display: flex;'>
+  <div class='swal2-success-circular-line-left' style='background-color: rgb(255, 255, 255);'></div>
+  <span class='swal2-success-line-tip'></span>
+  <span class='swal2-success-line-long'></span>
+  <div class='swal2-success-ring'></div>
+  <div class='swal2-success-fix' style='background-color: rgb(255, 255, 255);'></div>
+  <div class='swal2-success-circular-line-right' style='background-color: rgb(255, 255, 255);'></div>
+</div>
     แก้ไขเสร็จแล้ว <br><br>";
 }
 ?>
