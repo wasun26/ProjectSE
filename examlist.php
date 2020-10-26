7181937de743e2cbd6123f881310fad22db2002a
Wasun  Patrut <wasun26@hotmail.com>
2020-10-26 10:26:18 +0700

    Update examlist.php

--- examlist.php
+++ examlist.php
@@ -70,10 +70,10 @@ include("config.php");
             break;
           case 'examinerName':
             $input = $_POST['searchData'];
-            }
+            $name = explode(" ", $input);
             $sql = "SELECT E.id, E.subject, E.date, T.timeStart, T.timeFinish, E.room, P.name
             FROM timeexam T, exam E, phase P, user U
-            WHERE E.phase=P.id AND E.time=T.id AND (E.examiner_t=U.id OR E.examiner_s=U.id) AND ($nameCon)";
+            WHERE E.phase=P.id AND E.time=T.id AND (E.examiner_t=U.id OR E.examiner_s=U.id) AND (U.fanem LIKE ('%$name[0]%') OR U.lname LIKE ('%$name[1]%'))";
             break;
           case 'byterm':
             $phase = $_POST['phase'];
